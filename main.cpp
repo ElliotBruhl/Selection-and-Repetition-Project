bd8b8221a0bd040e1a9bc83acdec90e7cc4a5efa
ElliotBruhl <166664797+ElliotBruhl@users.noreply.github.com>
2024-09-23 14:17:47 +0000

    final

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
-  
+  // init
+  int number1;
+  int number2;
+  char operation;
+
+  // input
+  cout << "Enter the first number: ";
+  cin >> number1;
+  cout << "Enter the operator(+,-,*,/): ";
+  cin >> operation;
+  cout << "Enter the second number: ";
+  cin >> number2;
+
+  // output
+  switch (operation) {
+  case '+':
+    cout << number1 << " + " << number2 << " = " << number1 + number2;
+    break;
+  case '-':
+    cout << number1 << " - " << number2 << " = " << number1 - number2;
+    break;
+  case '*':
+    cout << number1 << " * " << number2 << " = " << number1 * number2;
+    break;
+  case '/':
+    if (number2 == 0) {
+      cout << "Error: Division by zero";
+      return 1;
+    }
+    cout << number1 << " / " << number2 << " = " << number1 / number2;
+    break;
+  default:
+    cout << "Error: Invalid operator";
+    return 1;
+  }
+
+  return 0;
 }
